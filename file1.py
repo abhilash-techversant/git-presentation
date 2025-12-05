4f6bd8bdd2b5197cc876dde336a120a040ea0301
abhilash-techversant <abhilash.antony@techversantinfotech.com>
2025-12-05 10:01:55 +0530

    initial commit

---
+++ file1.py
@@ -0,0 +1,9 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    return a + b
+
+print(greet("Abhilash"))
+print("Sum:", add(3, 5))
+
---
+++ file2.py
@@ -0,0 +1,9 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+def add(a, b):
+    result = a + b
+    return result
+
+print(greet("Abhilash"))
+print("Total:", add(3, 5))
